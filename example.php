71a594609f100ff772048521a657bba0082b7489
Martin Kovar <mkovar86@gmail.com>
2013-12-09 22:18:20 +0100

    first example

---
+++ example.php
@@ -0,0 +1,43 @@
+<?php
+include 'Performance/Profiler/Monitor.php';
+Performance_Profiler_Monitor::getInstance()->enable();
+declare(ticks=1);
+
+echo "hello";
+
+a();
+
+sleep(1);
+
+Performance_Profiler_Monitor::getInstance()->disable();
+
+
+function a() {
+    for($i = 0; $i < 4; $i++) {
+        b();
+    }
+
+    $arr = array(1,2);
+    $test = 0;
+
+    foreach ($arr as $a) {
+        while($test < 1) {
+            $test++;
+        }
+
+        echo $a;
+    }
+}
+
+function b() {
+    echo 'start b';
+    usleep(20000);
+    c();
+    usleep(20000);
+    echo 'end b';
+}
+
+function c() {
+    echo 'c';
+    usleep(50000);
+}
