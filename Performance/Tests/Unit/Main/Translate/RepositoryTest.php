480bece4027ddb108cb4f3eaae56b0d905d6bd2e
Martin Kovar <mkovar86@gmail.com>
2014-02-06 17:02:27 +0100

    unit tests

---
+++ Performance/Tests/Unit/Main/Translate/RepositoryTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace PF\Tests\Unit\Main\Translate;
+
+use PF\Main\Abstracts\Unit\TestCase;
+
+/**
+ * This script defines class for php unit test case of class \PF\Main\Translate\Repository.
+ *
+ * @author     Martin Kovar
+ * @category   Performance
+ * @package    Tests
+ */
+class RepositoryTest extends TestCase {
+
+    /**
+     * Access instance.
+     *
+     * @var \PF\Main\Translate\Repository
+     */
+    private $_repository;
+
+    /**
+     * Init method for create instances and basic setup.
+     *
+     * @return void
+     */
+    protected function setUp() {
+        $this->_repository = $this->getProvider()->get('PF\Main\Translate\Repository');
+
+        parent::setUp();
+    }
+
+    /**
+     * Success test for get all translation.
+     *
+     * @return void
+     */
+    public function testGetAll() {
+        $translate = $this->_repository->getTranslateTable();
+
+        $this->assertCount(2, $translate);
+    }
+
+    /**
+     * Success test for get all translation from CS lang.
+     *
+     * @return void
+     */
+    public function testGetAllCS() {
+        $translate = $this->_repository->getTranslateTable('CS');
+
+        $this->assertCount(3, $translate);
+    }
+
+    /**
+     * Success test for get all translation from CS lang and module main.
+     *
+     * @return void
+     */
+    public function testGetModule() {
+        $translate = $this->_repository->getTranslateTable('CS', 'main');
+
+        $this->assertCount(1, $translate);
+    }
+
+    /**
+     * Success test for get all translation from EN lang and module main.
+     *
+     * @return void
+     */
+    public function testGetModuleEN() {
+        $translate = $this->_repository->getTranslateTable('EN', 'main');
+
+        $this->assertCount(0, $translate);
+    }
+}
