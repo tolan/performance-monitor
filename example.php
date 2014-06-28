2739296902a1e6da4a617ed3d7dd5d0bd9eb2e93
Martin Kovar <mkovar86@gmail.com>
2014-06-28 11:47:27 +0200

    #5 - bugfix - fix for detecting "if" statement #6 - bugfix - fix for detecting reflection method

--- example.php
+++ example.php
@@ -8,11 +8,14 @@ declare(ticks=1);
 
     (new Foo())->bar(20);
 
+    test_if(true);
+    test_if(false);
+
     sleep(3);
 \PF\Profiler\Monitor::getInstance()->disable()->display();
 
 function simple ($input) {
-    for($i = 0; $i < 2000; $i++) {
+    for($i = 0; $i < 2; $i++) {
         $input = sub($input, $i);
         $time = time();
     }
@@ -29,4 +32,17 @@ class Foo {
     public function bar($input) {
         echo '<div>Foo->bar('.$input.'): '.simple($input).'</div>';
     }
+
+    public function test() {
+        usleep(10000);
+        class_exists('Foo');
+    }
+}
+
+function test_if ($cond) {
+    if ($cond == true) {
+        $ref = new ReflectionMethod('Foo', 'test');
+        $ref->invokeArgs(new Foo(), array());
+    }
+
 }
